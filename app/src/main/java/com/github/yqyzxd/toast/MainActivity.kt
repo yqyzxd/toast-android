02ba18adc08757919a6f0a8d3dab59a81eb3f7d7
windq <shihaowind@163.com>
2022-01-19 09:12:57 +0800

    toast增加padding

--- app/src/main/java/com/github/yqyzxd/toast/MainActivity.kt
+++ app/src/main/java/com/github/yqyzxd/toast/MainActivity.kt
@@ -2,10 +2,13 @@ package com.github.yqyzxd.toast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wind.toastlib.ToastUtil
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ToastUtil.showToast(this,"哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈")
     }
 }
