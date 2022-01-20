0bceacf940eb7d9b94d3a28526f4be0fd8cd3c2b
windq <shihaowind@163.com>
2022-01-20 16:39:21 +0800

    toast增加高度

--- app/src/main/java/com/github/yqyzxd/toast/MainActivity.kt
+++ app/src/main/java/com/github/yqyzxd/toast/MainActivity.kt
@@ -9,6 +9,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ToastUtil.showToast(this,"哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈")
+        ToastUtil.showToast(
+            this,
+            "健身房生活方式开发商康师傅还是开发客户是客服回复是客服核实客户富士康富士康还是符合事实付款后是否符合是否还会发回复时候发货后恢复"
+        )
+
     }
 }
